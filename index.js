6100c881233e891724cdf37e1aa56e41a2a0e944
saswat <saswatkumarpanda515@gmail.com>
2022-02-01 11:21:27 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+const path = require("path");
+
+const port = process.env.PORT || 3000
+
+const staticPath= path.join(__dirname,'./static')
+app.use(express.static(staticPath));
+
+app.listen(port,() => {
+  console.log(`Server running at ${port}`);
+});
